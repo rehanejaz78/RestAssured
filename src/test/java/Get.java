05ddb55364c35c3936fbdc7c07b3d9edbea22f10
rehanejaz78 <82099442+rehanejaz78@users.noreply.github.com>
2022-09-19 18:11:16 +0500

    GET API

---
+++ src/test/java/Get.java
@@ -0,0 +1,35 @@
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Get {
+
+    
+    @Test
+    public static void GET_Test(){
+
+        RestAssured.baseURI= "https://reqres.in/api";
+
+        int code =given().log().all()
+                .when().get("users?page=2").then().log().all().statusCode(200).extract().response().statusCode();
+
+        System.out.println(" Status code of this GET api is : "+code);
+
+    }
+}
+
+/*
+* RestAssured.baseURI = "https://reqres.in/api";
+        given().queryParam("token", "QpwL5tke4Pnpja7X4")
+
+                .log().all()
+                .body("{\n" +
+                        "    \"email\": \"eve.holt@reqres.in\",\n" +
+                        "    \"password\": \"cityslicka\"\n" +
+                        "}")
+                .when().post("/login")
+                .then().log().all()
+                .statusCode(200)
+                .extract().response().asString();
+* */
