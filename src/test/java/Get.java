f9c23af1fe4a46d464ede505bfcc40a1cee70131
rehanejaz78 <82099442+rehanejaz78@users.noreply.github.com>
2022-09-19 19:35:33 +0500

    Final commit of assignment 2

--- src/test/java/Get.java
+++ src/test/java/Get.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -6,7 +7,8 @@ import static io.restassured.RestAssured.given;
 public class Get {
 
 
-    @Test
+
+    @Test(priority = 1)
     public static void GET_Test(){
 
         RestAssured.baseURI= "https://reqres.in/api";
@@ -15,22 +17,64 @@ public class Get {
                 .when().get("users?page=2").then().log().all().statusCode(200).extract().response().statusCode();
 
         System.out.println(" Status code of this GET api is : "+code);
-        System.out.println("kjchs");
+
 
     }
+
+    @Test(priority = 2)
+    public void POST_test(){
+        RestAssured.baseURI= "https://reqres.in/api";
+
+        String res = given().body("{\n" +
+                "    \"name\": \"rehan ejaz\",\n" +
+                "    \"job\": \"leader\"\n" +
+                "}").post("/users").then().log().all().statusCode(201).log().all().extract().response().asString() ;
+
+
+
+        JsonPath jsonPath = new JsonPath(res);
+
+        String id = jsonPath.get("id");
+        System.out.println(id);
+        System.out.println("Record posted successfully");
+
+        PUT_Test(id);
+
+    }
+
+
+    public void PUT_Test(String id) {
+
+        RestAssured.baseURI= "https://reqres.in/api";
+        given().log().all()
+                .body("{\n" +
+                        "    \"name\": \"rehan\",\n" +
+                        "    \"job\": \"automation\"\n" +
+                        "}")
+                .when().put("/users/" + id + "")
+                .then().log().all()
+                .assertThat().statusCode(200)
+                .extract().response().asString();
+        System.out.println("Record Updated Successfully !!");
+
+        Delete(id);
+
+    }
+
+    public void Delete(String id) {
+
+        RestAssured.baseURI= "https://reqres.in/api";
+
+
+        given().log().all()
+                .when().delete("/users/" + id + "")
+                .then().log().all()
+                .assertThat().statusCode(204)
+                .extract().response().asString();
+        System.out.println("Record deleted successfully !");
+
+    }
+
+
 }
 
-/*
-* RestAssured.baseURI = "https://reqres.in/api";
-        given().queryParam("token", "QpwL5tke4Pnpja7X4")
-
-                .log().all()
-                .body("{\n" +
-                        "    \"email\": \"eve.holt@reqres.in\",\n" +
-                        "    \"password\": \"cityslicka\"\n" +
-                        "}")
-                .when().post("/login")
-                .then().log().all()
-                .statusCode(200)
-                .extract().response().asString();
-* */
